88d9546c51707135303dabce31259deb7757c097
Nat-Dan <nat.dan@mailfence.com>
2022-05-23 15:10:33 +0300

    feat: add core-js-101 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -129,8 +129,11 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const LastDigit = String(value).slice(-1);
+  const lastDigitNum = Number(LastDigit);
+  return lastDigitNum;
+  // throw new Error('Not implemented');
 }
 
 
@@ -145,8 +148,10 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  const parseNumber = Number(value);
+  return parseNumber;
+  // throw new Error('Not implemented');
 }
 
 /**
